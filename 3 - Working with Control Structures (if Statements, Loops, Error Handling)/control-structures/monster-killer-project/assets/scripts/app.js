d085dd561f0f55960ff1c1aea3b4b82ffae7dcb0
hishamrehab <hishamrehab30@gmail.com>
2025-10-26 12:56:05 +0300

    For of loop

--- 3 - Working with Control Structures (if Statements, Loops, Error Handling)/control-structures/monster-killer-project/assets/scripts/app.js
+++ 3 - Working with Control Structures (if Statements, Loops, Error Handling)/control-structures/monster-killer-project/assets/scripts/app.js
@@ -222,7 +222,18 @@ function printLogHandler() {
   //   console.log('---------------------');
   //   console.log(i);
   // }
-console.log(battleLog);
+
+  //  for(let i = 0 ; i < battleLog.length; i++) {
+  //    console.log(battleLog[i]);
+  //  }
+
+  //   console.log(battleLog);
+  let i = 0;
+  for(const logEntry of battleLog) {
+    console.log(logEntry);
+    console.log("i");
+    i++;
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
