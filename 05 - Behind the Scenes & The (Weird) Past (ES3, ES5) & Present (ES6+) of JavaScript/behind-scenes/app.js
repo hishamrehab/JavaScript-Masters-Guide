6d6596977162e021a5e1fabe1112d2093bbc3208
hishamrehab <hishamrehab30@gmail.com>
2025-10-30 17:55:31 +0300

    Es6 - let , var, const

---
+++ 05 - Behind the Scenes & The (Weird) Past (ES3, ES5) & Present (ES6+) of JavaScript/behind-scenes/app.js
@@ -0,0 +1,16 @@
+ var name = "Max";
+ let ho
+
+ if(name === "Max") {
+     let hobbies = ["Sports", "Cooking"];
+     console.log(hobbies);
+ }
+
+function greet() {
+    var age = 30;
+    let name = "Anula";
+    console.log(name , age , hobbies);
+}
+
+console.log(age , name);
+greet();
