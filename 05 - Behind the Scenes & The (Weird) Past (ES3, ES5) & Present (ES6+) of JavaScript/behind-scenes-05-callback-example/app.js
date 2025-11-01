f88411e88e45c6a81557aa14de81fe3ca609b18b
hishamrehab <hishamrehab30@gmail.com>
2025-11-01 23:29:54 +0200

    Garbage Collection & Memory Management

--- 05 - Behind the Scenes & The (Weird) Past (ES3, ES5) & Present (ES6+) of JavaScript/behind-scenes-05-callback-example/app.js
+++ 05 - Behind the Scenes & The (Weird) Past (ES3, ES5) & Present (ES6+) of JavaScript/behind-scenes-05-callback-example/app.js
@@ -2,6 +2,14 @@ const addListenerBtn = document.getElementById('add-listener-btn');
 const clickableBtn = document.getElementById('clickable-btn');
 const messageInput = document.getElementById('click-message-input');
 
+
+let person = {name : "Max"};
+
+person = null;
+
+
+
+
 function printMessage() {
   const value = messageInput.value;
   console.log(value || 'Clicked me!');
