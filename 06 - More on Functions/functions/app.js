788504a816520c4db6591f815ff23205e81ffc5c
hishamrehab <hishamrehab30@gmail.com>
2025-11-19 14:58:13 +0200

    Adding Pind Function

--- 06 - More on Functions/functions/app.js
+++ 06 - More on Functions/functions/app.js
@@ -82,24 +82,46 @@ startGameBtn.addEventListener('click', () => {
 });
 
 // not related to game
-
-const sumUp = (resultHander, ...numbers) => {
-   const validateNumber = (number) => {
+const combine = (
+  resultHander,
+   operation ,
+   ...numbers) => {
+  const validateNumber = (number) => {
      return isNaN(number) ? 0 : number;
   };
 
   let sum = 0;
   for (const num of numbers) {
-    sum += validateNumber(num);
+    if(operation === "ADD"){
+      sum += validateNumber(num);
+    }else {
+      sum -= validateNumber(num);
+    }
   }
   resultHander(sum);
 };
 
-const showResult = (result) => {
-  alert("The result after adding all number is:" + result);
-}
+
+// const subtractUp = function(resultHander , ...numbers) {
+//   let sum = 0;
+//   for (const num of numbers) {
+//     sum -=num;
+//   }
+//   resultHander(sum , "The result after adding all numbers is");
+// };
 
 
+const showResult = (messageText , result) => {
+  alert(messageText + ' ' + result);
+};
 
-sumUp(showResult, 1, 5, 10, -3, 6, 10);
-console.log(sumUp(showResult,1, 5, 10, -3, 6, 10, 25, 88));
+combine(showResult.bind(this,
+"The result after adding all numbers is:"),
+"ADD" ,  1, 5, 10, -3, 6, 10);
+combine(
+  showResult.bind(this,
+  "The result after adding all numbers is:"),
+  "ADD" ,  1, 5, 10, -3, 6, 10);
+combine(showResult.bind(this,
+   "The result after subtracting all numbers is: "),
+   "ADD" ,  1, 5, 10, -3, 6, 10);
