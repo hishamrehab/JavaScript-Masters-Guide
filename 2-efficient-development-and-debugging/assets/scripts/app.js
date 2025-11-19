3c7b83fc2544092d875758fe4ad02f558a99b2ee
hishamrehab <hishamrehab30@gmail.com>
2025-11-19 15:19:41 +0200

    Adding bind method to the calculator function

--- 2-efficient-development-and-debugging/assets/scripts/app.js
+++ 2-efficient-development-and-debugging/assets/scripts/app.js
@@ -30,44 +30,30 @@
    }
 
 
-    // Fuction add the numbers
-    function add() {
-        const enterdNumber = getUserNumberInput();
-         const initialResult = currentResult;
-        currentResult +=enterdNumber ;
-        createAddWriteOutput("+", initialResult, enterdNumber);
-      
-        writeToLog("ADD", initialResult, enterdNumber, currentResult);
-    }
+   function claculate(operation) {
+  const enterdNumber = getUserNumberInput();
+      const initialResult = currentResult;
+    let operator;
+     if(operation === "ADD") {
+       currentResult +=enterdNumber;
+       operator = "+";
+     } else if(operation === "SUBTRACT") {
+       currentResult -=enterdNumber ;
+      operator = "-";  
+      } else if(operation === "MULTIPLY") {
+      currentResult *=enterdNumber ;
+     operator = "*";
+     } else if(operation === "DIVIDE") {
+     currentResult -=enterdNumber ;
+      operator = "-";
+     }
+     createAddWriteOutput(operator, initialResult, enterdNumber);
+      writeToLog(operation, initialResult, enterdNumber, currentResult);
+   }
 
- // Fuction subtract the numbers
-    function subtract() {
-        const enterdNumber = getUserNumberInput();
-        const initialResult = currentResult;
-        currentResult -=enterdNumber ;
-        createAddWriteOutput("-", initialResult, enterdNumber); 
-        writeToLog("SUBTRACT", initialResult, enterdNumber, currentResult);
+     
 
-    }
-  
-
-    function multiply() {
-         const enterdNumber = getUserNumberInput();
-        const initialResult = currentResult;
-        currentResult *=enterdNumber ;
-        writeToLog("MULTIPLY", initialResult, enterdNumber, currentResult);
-        typeof("number");
-    }
-
-    function divide() {
-      const enterdNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    currentResult /= enterdNumber ;
-    writeToLog("DIVIDE", initialResult, enterdNumber, currentResult);
-    }
-
-
-    addBtn.addEventListener("click", add);
-    subtractBtn.addEventListener("click", subtract);
-    multiplyBtn.addEventListener("click", multiply);
-    divideBtn.addEventListener("click", divide);
+    addBtn.addEventListener("click", claculate.bind(this , "ADD"));
+    subtractBtn.addEventListener("click",claculate.bind(this , "SUBTRACT"));
+    multiplyBtn.addEventListener("click", claculate.bind(this , "MULTYPLY"));
+    divideBtn.addEventListener("click", claculate.bind(this , "DIVIDE"));
